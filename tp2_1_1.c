4dd4625d7926240f850335d33c95baf3fc004b0b
Joaquin <joaquinlechesi@gmail.com>
2025-04-01 12:06:40 -0300

    inicial

---
+++ tp2_1_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 20 
+
+// codigo a completar 
+int main()
+{
+    /* code */
+    
+    int i; 
+    double vt[N]; 
+    srand(time(NULL));
+    for(i = 0;i<N; i++) 
+    {  
+        vt[i]=1+rand()%100;
+        printf("%.2f   ", vt[i]);     
+    }
+    return 0;
+}
