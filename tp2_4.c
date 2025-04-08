89b251bcd05b1bf934e80988fbce8238c174a93d
Joaquin <joaquinlechesi@gmail.com>
2025-04-07 22:26:35 -0300

    tp2_4

--- tp2_4.c
+++ tp2_4.c
@@ -12,6 +12,8 @@ struct compu
 typedef struct compu Compu;
 
 void listaPCs(Compu arreglo[], int longitud);
+void mostrarMasVieja(Compu arreglo[], int longitud);
+void mostrarMasVeloz(Compu arreglo[], int longitud);
 
 int main()
 {
@@ -27,6 +29,8 @@ int main()
         listaCompu[i].tipo_cpu = tipos[rand()%5];
     }
     listaPCs(listaCompu, TAMA);
+    mostrarMasVieja(listaCompu, TAMA);
+    mostrarMasVeloz(listaCompu, TAMA);
     
     return 0;
 }
@@ -35,6 +39,36 @@ void listaPCs(Compu arreglo[], int longitud){
     puts("\tFUNCION LISTAR PCs:");
     for (size_t i = 0; i < longitud; i++)
     {
-        printf("  Computadora numero: %d\nNombre: %s\nAño: %d\nVelocidad: %d\nCantidad de nucleos: %d\n", i+1, arreglo[i].tipo_cpu, arreglo[i].anio, arreglo[i].velocidad, arreglo[i].cantidad);
+        printf("  Computadora numero: %d\nNombre: %s\nAño: %d\nVelocidad: %d Ghz\nCantidad de nucleos: %d\n", i+1, arreglo[i].tipo_cpu, arreglo[i].anio, arreglo[i].velocidad, arreglo[i].cantidad);
     }
+}
+
+void mostrarMasVieja(Compu arreglo[], int longitud){
+    puts("\tFUNCION PC MAS ANTIGUA:");
+    int posicion = 0;
+    int anio = arreglo[0].anio;
+    for (size_t i = 0; i < longitud; i++)
+    {
+        if (arreglo[i].anio < anio)
+        {
+            anio = arreglo[i].anio;
+            posicion = i;
+        }
+    }
+    printf("Nombre: %s\nAño: %d\nVelocidad: %d Ghz\nCantidad de nucleos: %d", arreglo[posicion].tipo_cpu, arreglo[posicion].anio, arreglo[posicion].velocidad, arreglo[posicion].cantidad);
+}
+
+void mostrarMasVeloz(Compu arreglo[], int longitud){
+    puts("\n\tFUNCION PC MAS VELOZ");
+    int posicion;
+    int velocidad = arreglo[0].velocidad;
+    for (size_t i = 0; i < longitud; i++)
+    {
+        if (arreglo[i].velocidad > velocidad)
+        {
+            velocidad = arreglo[i].velocidad;
+            posicion = i;
+        }
+    }
+    printf("Nombre: %s\nAño: %d\nVelocidad: %d Ghz\nCantidad %d", arreglo[posicion].tipo_cpu, arreglo[posicion].anio, arreglo[posicion].velocidad, arreglo[posicion].cantidad);
 }
